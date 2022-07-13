abb9847b77ce6ea0fa078588ecb28d3fb65efd65
Ulrich Fechtelhoff <ulrich.fechtelhoff@iqtig.org>
2022-07-13 14:02:07 +0200

    Dependency Version Updates.

--- create-java.sh
+++ create-java.sh
@@ -13,14 +13,30 @@ else
   ARTIFACT="$1"
 fi
 
-if [ -z "$1" ]
+if [ -z "$2" ]
 then
   GROUP="de.fechtelhoff"
 else
   GROUP="$2"
 fi
 
-mvn archetype:generate --batch-mode -DarchetypeGroupId=de.fechtelhoff -DarchetypeArtifactId=maven-template -DarchetypeVersion=1.0-SNAPSHOT -DgroupId=${GROUP} -DartifactId=${ARTIFACT}
+VERSION="1.0-SNAPSHOT"
+PACKAGE="${GROUP}"."${ARTIFACT}"
+
+echo "Artifact ID = ${ARTIFACT}"
+echo "Group ID = ${GROUP}"
+echo "Version = ${VERSION}"
+echo "Package = ${PACKAGE}"
+echo
+
+mvn archetype:generate --batch-mode \
+  -DarchetypeGroupId=de.fechtelhoff \
+  -DarchetypeArtifactId=maven-template \
+  -DarchetypeVersion=1.0-SNAPSHOT \
+  -DgroupId="${GROUP}" \
+  -DartifactId="${ARTIFACT}" \
+  -Dversion="${VERSION}" \
+  -Dpackage="${PACKAGE}"
 
 echo
 echo "###############################################################################"
